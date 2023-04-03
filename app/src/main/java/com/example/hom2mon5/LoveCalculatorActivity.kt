2759289b8f586ad2386bb8b9d81be7a8d758c700
fruty809 <brawlstarsvoron809@gmail.com>
2023-04-03 15:33:11 +0600

    remake

--- app/src/main/java/com/example/hom2mon5/LoveCalculatorActivity.kt
+++ app/src/main/java/com/example/hom2mon5/LoveCalculatorActivity.kt
@@ -1,69 +1,38 @@
-import androidx.appcompat.app.AppCompatActivity
+package com.example.hom2mon5
+
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import com.example.hom2mon5.LoveCalculatorApi
-import com.example.hom2mon5.LoveCalculatorResponse
-import com.example.hom2mon5.R
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import androidx.appcompat.app.AppCompatActivity
 
 class LoveCalculatorActivity : AppCompatActivity() {
-
-    private lateinit var firstNameEditText: EditText
-    private lateinit var secondNameEditText: EditText
-    private lateinit var calculateButton: Button
-    private lateinit var resultTextView: TextView
-
-    private val BASE_URL = "https://love-calculator.p.rapidapi.com/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_love_calculator)
 
-        firstNameEditText = findViewById(R.id.firstNameEditText)
-        secondNameEditText = findViewById(R.id.secondNameEditText)
-        calculateButton = findViewById(R.id.calculateButton)
-        resultTextView = findViewById(R.id.resultTextView)
+        // Set up button click listener
+        val buttonCalculate = findViewById<Button>(R.id.buttonCalculate)
+        buttonCalculate.setOnClickListener {
+            // Get the input values
+            val person1Name = findViewById<EditText>(R.id.editTextPerson1).text.toString().trim()
+            val person2Name = findViewById<EditText>(R.id.editTextPerson2).text.toString().trim()
 
-        calculateButton.setOnClickListener {
-            val firstName = firstNameEditText.text.toString()
-            val secondName = secondNameEditText.text.toString()
+            // Calculate love score
+            val loveScore = calculateLoveScore(person1Name, person2Name)
 
-            val retrofit = getRetrofitInstance()
-
-            val loveCalculatorApi = retrofit.create(LoveCalculatorApi::class.java)
-
-            loveCalculatorApi.getPercentage(firstName, secondName).enqueue(object : Callback<LoveCalculatorResponse> {
-                override fun onResponse(
-                    call: Call<LoveCalculatorResponse>,
-                    response: Response<LoveCalculatorResponse>
-                ) {
-                    val percentage = response.body()?.percentage
-
-                    if (percentage != null) {
-                        val result = "Your love percentage is: $percentage%"
-                        resultTextView.text = result
-                    } else {
-                        resultTextView.text = "Error getting love percentage"
-                    }
-                }
-
-                override fun onFailure(call: Call<LoveCalculatorResponse>, t: Throwable) {
-                    resultTextView.text = "Error getting love percentage"
-                }
-            })
+            // Start com.example.hom2mon5.ResultActivity with the love score and input names
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("person1Name", person1Name)
+            intent.putExtra("person2Name", person2Name)
+            intent.putExtra("loveScore", loveScore)
+            startActivity(intent)
         }
     }
 
-    private fun getRetrofitInstance(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private fun calculateLoveScore(person1Name: String, person2Name: String): Int {
+        // TODO: Implement love score calculation
+        return 50 // default value
     }
 }
--- app/src/main/java/com/example/hom2mon5/ResultActivity.kt
+++ app/src/main/java/com/example/hom2mon5/ResultActivity.kt
@@ -1,18 +1,24 @@
 package com.example.hom2mon5
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
-        val result = intent.getStringExtra("RESULT")
-        resultTextView.text = result
+        // Get the input values from the intent extras
+        val person1Name = intent.getStringExtra("person1Name")
+        val person2Name = intent.getStringExtra("person2Name")
+        val loveScore = intent.getIntExtra("loveScore", 0)
+
+        // Set the result text
+        val textViewResult = findViewById<TextView>(R.id.textViewResult)
+        textViewResult.text = getString(R.string.result_text, person1Name, person2Name, loveScore)
     }
 }
